fc569ddaf6582b675f2c0f2816e0f5e1e2a3932d
yeopyeop-82 <a01055143647@gmail.com>
2023-11-06 15:29:34 +0900

    section 8 finish

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -26,5 +29,17 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    @PostConstruct
+    public void init() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() throws Exception {
+        System.out.println("destroy");
+        disconnect();
     }
 }
